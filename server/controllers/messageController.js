2344ef36478b55bed1a6dc4c6875485676e8fbeb
Suhana Pendhari <suhanapendhari786@gmail.com>
2026-02-17 19:58:34 +0530

    backend-done

--- server/controllers/messageController.js
+++ server/controllers/messageController.js
@@ -80,3 +80,37 @@ export const sendMessage = async (req, res) => {
         res.json({ success: false, message: error.message });
     }
 }
+
+// Get Chat Messages
+export const getChatMessages = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { to_user_id } = req.body;
+
+    const messages = await Message.find({
+      $or: [
+        {from_user_id: userId, to_user_id},
+        {from_user_id: to_user_id, to_user_id: userId},
+      ]
+    }).sort({created_at: -1})
+    // mark messages as seen
+    await Message.updateMany({from_user_id: to_user_id, to_user_id:
+    userId}, {seen: true})
+
+    res.json({ success: true, messages });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
+
+export const getUserRecentMessages = async (req, res) => {
+  try {
+    const { userId } = req.auth();
+    const { messages } = await Message.find({to_user_id: userId}.populate
+    ('from_user_id to_user_id')).sort({ created_at: -1 });
+
+    res.json({ success: true, messages });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+}
